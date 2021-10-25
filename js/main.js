736b44e70649c30b812501c005c74ca2c1636a72
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-10-25 23:24:55 +0900

    애니메이션 시작과 끝지점 정하기

--- js/main.js
+++ js/main.js
@@ -18,7 +18,8 @@
         messageD: document.querySelector("#scroll-section-0 .main-message.d"),
       },
       values: {
-        messageA_opacity: [0, 1], // messageA의 투명도를 담음 내릴때 0~1
+        messageA_opacity: [0, 1, { start: 0.1, end: 0.2 }], // messageA의 투명도를 담음 내릴때 0~1
+        messageB_opacity: [0, 1, { start: 0.3, end: 0.4 }],
       },
     },
     // 1
@@ -75,8 +76,31 @@
   function calcValues(values, currentYOffset) {
     let rv;
     // 현재씬 (스코롤섹션)에서 스크롤된 범위를 비율로 구하기
-    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
-    rv = scrollRatio * (values[1] - values[0]) + values[0];
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+
+    if (values.length === 3) {
+      // start ~ end 사이에 애니메이션 실행
+      const partScrollStart = values[2].start * scrollHeight;
+      const partScrollEnd = values[2].end * scrollHeight;
+      const partScrollHeight = partScrollEnd - partScrollStart;
+
+      if (
+        currentYOffset >= partScrollStart &&
+        currentYOffset <= partScrollEnd
+      ) {
+        rv =
+          ((currentYOffset - partScrollStart) / partScrollHeight) *
+            (values[1] - values[0]) +
+          values[0];
+      } else if (currentYOffset < partScrollStart) {
+        rv = values[0];
+      } else if (currentYOffset > partScrollEnd) {
+        rv = values[1];
+      }
+    } else {
+      rv = scrollRatio * (values[1] - values[0]) + values[0];
+    }
     return rv;
   }
 
